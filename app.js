db3b2fbea3a65fccfcae942c50495c8ffc976bdb
hariprakashkhp <hariprakash.khp@gmail.com>
2023-03-11 08:28:08 +0530

    Completed the functinality of post app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,13 +14,23 @@ const contactContent =
 
 const app = express();
 
+const Posts = [
+  {
+    postTitle: "Home",
+    postBody:
+      "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+  },
+];
+
+app.locals._ = _;
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs", { homeStartingContent });
+  res.render("home.ejs", { Posts });
 });
 
 app.get("/about", (req, res) => {
@@ -28,6 +39,29 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("contact.ejs", { contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose.ejs");
+});
+
+app.get("/posts/:title", (req, res) => {
+  let title = req.params.title;
+  let postData = Posts.filter((post) => {
+    return _.lowerCase(title) === _.lowerCase(post.postTitle);
+  });
+  let data = postData[0];
+  res.render("post.ejs", { data });
+});
+
+app.post("/compose", (req, res) => {
+  const { postTitle, postBody } = req.body;
+  const post = {
+    postTitle,
+    postBody,
+  };
+  Posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
